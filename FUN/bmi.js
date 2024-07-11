f0eb67cccf28d84dffce280e54682c6e42849e12
nithin987u <159122746+nithin987u@users.noreply.github.com>
2024-07-11 09:21:45 +0530

    Add files via upload

---
+++ FUN/bmi.js
@@ -0,0 +1,21 @@
+function calculateBMI()
+{
+    var height = parseFloat(document.getElementById("height").value);
+    var weight = parseFloat(document.getElementById("weight").value);
+    
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        document.getElementById("result").innerText = "Please Enter valid values.";
+        return;
+    }
+var bmi = weight / Math.pow(height / 100, 2);
+document.getElementById("result").innerText="Your BMI is:" +bmi.toFixed(2);
+}
+
+function Clear() {
+    document.getElementById("height").value = "";
+    document.getElementById("weight").value = "";
+    document.getElementById("result").innerText = "";
+}
+
+
+
